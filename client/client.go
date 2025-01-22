c4e72ec45ee0cbffb99f7247c37ff74722a803da
xmkdgdz <xmkdgdz@foxmail.com>
2025-01-22 15:01:02 +0800

    Update: 增加客户端操作

--- client/client.go
+++ client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -11,12 +13,14 @@ type Client struct {
 	ServerPort int
 	Name string
 	conn net.Conn
+	flag int // 当前客户端模式
 }
 
 func NewClient(serverIP string, serverPort int) *Client {
 	client := &Client{
 		ServerIP: serverIP,
 		ServerPort: serverPort,
+		flag: 999,
 	}
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIP, serverPort))
@@ -28,6 +32,120 @@ func NewClient(serverIP string, serverPort int) *Client {
 	client.conn = conn
 
 	return client
+}
+
+// 处理服务端响应
+func (client *Client) DealResponse() {
+	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("---------欢迎使用即时通讯系统---------")
+	fmt.Println("1. 公聊模式")
+	fmt.Println("2. 私聊模式")
+	fmt.Println("3. 更新用户名")
+	fmt.Println("0. 退出系统")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("输入错误，请重新输入数字")
+		return false
+	}
+}
+
+func (client *Client) PublicChat() {
+	var chatMsg string
+	fmt.Println("请输入聊天内容，输入exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn.Write err: ", err)
+				break
+			}
+		}
+
+		chatMsg = ""
+		fmt.Println("请输入聊天内容，输入exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err: ", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var chatMsg string
+	var remoteName string
+
+	client.SelectUsers()
+	fmt.Println("请输入聊天对象用户名，输入exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println("请输入聊天内容，输入exit退出")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn.Write err: ", err)
+					break
+				}
+			}
+
+			chatMsg = ""
+			fmt.Println("请输入聊天内容，输入exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println("请输入聊天对象用户名，输入exit退出")
+		fmt.Scanln(&remoteName)
+	}
+}
+
+func (client *Client) UpdateName() bool{
+	fmt.Println("请输入用户名：")
+	fmt.Scanln(&client.Name)
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn.Write err: ", err)
+		return false
+	}
+	return true
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		if client.menu() {
+			switch client.flag {
+			case 1:
+				client.PublicChat()
+			case 2:
+				client.PrivateChat()
+			case 3:
+				client.UpdateName()
+			default:
+			}
+		}
+	}
 }
 
 var serverIP string
@@ -49,7 +167,9 @@ func main() {
 		return
 	}
 
+	go client.DealResponse()
+
 	fmt.Println("NewClient success")
 
-	select {}
+	client.Run()
 }
